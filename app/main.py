b4697111b95eb9c0e0cb2a636a125cd2676be14f
wofvh <wofvh@naver.com>
2022-08-25 16:48:46 +0900

    doker

---
+++ app/main.py
@@ -0,0 +1,21 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+basedir = os.path.dirname(os.path.abspath(__file__))
+
+from app.services.calculator import CalculatorService
+
+def print_menu():
+    print("0. 전체종료프로그램 종료") 
+    print("1. 계산기 프로그램") 
+     
+def main():
+    print_menu()
+    calculatorService = CalculatorService()
+    while 1 :
+        meun = print_menu()
+        if meun == '0':
+            print("프로그램을 종료합니다.")
+                
+    if __name__ == '__main__':
+        main()
---
+++ app/models/calculator.py
@@ -0,0 +1,33 @@
+###c 
+def calculate():
+    pass
+class Calculator(object):
+    def __init__(self,first,second):
+        self.first = first
+        self.second = second
+        
+    def add(self):
+        result = self.first + self.second
+        return result
+    
+    def minus(self):
+        result = self.first - self.second
+        return result
+    
+    def mult(self):
+        result = self.first * self.second
+        return result
+    
+    def divide(self):
+        result = self.first / self.second
+        return result
+    
+    
+# c = calculate(a, b)
+# print("덧샘", c.add())
+# print("뺄샘", c.minus())
+# print("곱샘", c.mult())
+# print("나눗샘", c.div())
+    
+    
+    

276b7f5bb3dc233e86a1e48e4364caab8a2c6686
wofvh <wofvh@naver.com>
2022-08-29 16:59:20 +0900

    doker

---
+++ docker/mysql/initdb.d/schema.sql
@@ -0,0 +1,6 @@
+create table user(
+     id varchar(10) primary key,
+     password varchar(10),
+     name varchar(12),
+     age int
+     )
---
+++ docker/mysql/initdb.d/testdata.sql
@@ -0,0 +1,3 @@
+insert into user (id, password, name, age) values ('hong','pass','홍길덩',20);
+insert into user (id, password, name, age) values ('teddy','pass','홍길덩',25);
+insert into user (id, password, name, age) values ('shanice','pass','홍길덩',22);
